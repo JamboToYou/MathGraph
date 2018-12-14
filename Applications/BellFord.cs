9b1407a73fec07a63533889d6bf2ef9390ad9207
Jambo <jambotoyou@gmail.com>
2018-12-15 01:20:45 +0400

    added BellFord process

---
+++ Applications/BellFord.cs
@@ -0,0 +1,9 @@
+namespace MathGraph.Applications
+{
+	public static class BellFord
+	{
+		public static void Init() {}
+		public static void Run() {}
+		public static string DisplayResult() => null;
+	}
+}
